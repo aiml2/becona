c4ffce5bc2c91c42db46d7116075222370656051
Dieter Castel <dietercastel@gmail.com>
2017-10-30 16:38:20 +0100

    Added tensorboard testing code.

---
+++ code/TensorBoardTest.py
@@ -0,0 +1,19 @@
+models = ["FTMC_Xception_TBT"]
+datapaths = ["../B2split_"]
+eras = range(2)
+crossValSet = [2] 
+
+classes = {}
+for idx, modelName in enumerate(models):
+    module = __import__(modelName)
+    class_ = getattr(module,modelName)
+    classes[modelName] = class_
+
+print(classes)
+
+for cvIndex in crossValSet: 
+    for idx,modelName in enumerate(models):
+        modelInstance = classes[modelName]()
+        for era in eras:
+            print("########## TRAINING era:" + str(era) + ", cvindex" + str(cvIndex)+"of model " + modelName)
+            modelInstance.trainEra(era, datapaths[idx], cvIndex)
