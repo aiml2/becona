5bed9b69408d339e3a24cfffc587a164f8999106
Dieter Castel <dietercastel@gmail.com>
2018-11-13 22:14:04 +0100

    Added the augmentation I used for the presentation

--- save_augmentation.py
+++ save_augmentation.py
@@ -21,39 +21,40 @@ train_datagen = im.ImageDataGenerator(
 	channel_shift_range=10,
 	preprocessing_function=preprocess_input)
 
-val_datagen = im.ImageDataGenerator(
-        shear_range=0.2,
-        zoom_range=[0.8,1],
-        horizontal_flip=True,
-        vertical_flip=True,
-	width_shift_range=0.3,
-	height_shift_range=0.2,
-	rotation_range=360,
-	#zca_whitening=True,
-	channel_shift_range=10,
-	preprocessing_function=preprocess_input) 
-
+#val_datagen = im.ImageDataGenerator(
+#        shear_range=0.2,
+#        zoom_range=[0.8,1],
+#        horizontal_flip=True,
+#        vertical_flip=True,
+#	width_shift_range=0.3,
+#	height_shift_range=0.2,
+#	rotation_range=360,
+#	#zca_whitening=True,
+#	channel_shift_range=10,
+#	preprocessing_function=preprocess_input) 
+#
 train_generator = train_datagen.flow_from_directory(
         directory,
         target_size=(299, 299), #def size of Inception is 299x299
         batch_size=32,
         class_mode='categorical',
 	follow_links=True,
-	save_to_dir='augtrain',
+	save_to_dir='augSplit2ValPres',
+	#save_to_dir='augtrain',
 	save_prefix='aug_')
 
-print(train_generator.class_indices)
-
-validation_generator = val_datagen.flow_from_directory(
-        directory,
-        target_size=(299, 299),
-        batch_size=32,
-        class_mode='categorical',
-	follow_links=True,
-	save_to_dir='augval',
-	save_prefix='aug_')
-
-print(validation_generator.class_indices)
+#print(train_generator.class_indices)
+#
+#validation_generator = val_datagen.flow_from_directory(
+#        directory,
+#        target_size=(299, 299),
+#        batch_size=32,
+#        class_mode='categorical',
+#	follow_links=True,
+#	save_to_dir='augval',
+#	save_prefix='aug_')
+#
+#print(validation_generator.class_indices)
 
 i = 0
 iters = 2
@@ -62,8 +63,8 @@ for batch in train_generator:
 	i += 1
 	if i > iters: # save 20 images
 		break  # otherwise the generator would loop indefinitely
-i = 0
-for batch in validation_generator:
-	i += 1
-	if i > iters: # save 20 images
-		break  # otherwise the generator would loop indefinitely
+#i = 0
+#for batch in validation_generator:
+#	i += 1
+#	if i > iters: # save 20 images
+#		break  # otherwise the generator would loop indefinitely
